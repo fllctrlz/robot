1f4be03cc579d67425f0960a147aba41733d236f
fllctrlz <fllctrlzfll@gmail.com>
2019-11-29 19:23:00 -0500

    aa

--- cityshaper/missions/testoPesto.py
+++ cityshaper/missions/testoPesto.py
@@ -18,7 +18,7 @@ def run():
     #pidLineFollower.run(20, robot.FRONT_LEFT, 1, distance, robot.motorB, 500)
     #pidLineFollower.run(-20, robot.BACK_LEFT, 1, time, robot.timer, 15)
     #pidLineFollower.run(20, robot.FRONT_LEFT, -1, light, robot.FRONT_RIGHT, 90, 100)
-    robot.sleep(69)
+    robot.sleep(60)
 
 
 def light():
